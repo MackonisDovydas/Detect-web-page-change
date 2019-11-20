e748ada463e669ef8c3b96907f513a51e0e23d92
Dovydas Mackonis <dragongalas@gmail.com>
2019-11-20 14:17:49 +0200

    Changed to firefox and new URL tracker

--- Comparer.py
+++ Comparer.py
@@ -1,16 +1,25 @@
+
 import sys
 
-
 def main():
+    Template_URL = "https://is.vu.lt/pls/klevas/am$pd_reg_app.show?p_stud_id=219671&p_kalba_name=lt"
     f = open("Template.txt", "r")
     template = f.read()
     f.close()
     f = open("html.txt", "r")
     html = f.read()
     f.close()
-    if template == html:
-        sys.exit(0)
+    f = open("URL.txt", "r")
+    URL = f.read()
+    f.close()
+
+    if Template_URL == URL:
+        if template == html:
+            sys.exit(0)
+        else:
+            sys.exit(1)
     else:
+        print("Not right url")
         sys.exit(1)
 
 if __name__ == '__main__':
--- Scraper2.py
+++ Scraper2.py
@@ -1,26 +1,30 @@
 import netrc
+import pickle
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import time
 
 HOST = 'VU_login'
 secrets = netrc.netrc()
 USERNAME, account, PASSWORD = secrets.authenticators(HOST)
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--headless')
-driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
+options = Options()
+firefox_profile = FirefoxProfile()
+options.headless = True
+firefox_profile.set_preference("javascript.enabled", True)
+options.profile = firefox_profile
+driver = webdriver.Firefox(options=options)
 driver.get("https://is.vu.lt")
 
 time.sleep(1)
 button = driver.find_element_by_xpath("//input[@name='Submit']")
 button.click()
 
-time.sleep(1)
 button = driver.find_element_by_xpath("//input[@name='src-dnVfbGRhcC1zdHVkZW50YWk=']")
 button.click()
 
+time.sleep(1)
 username = driver.find_element_by_xpath("//input[@name='username']")
 password = driver.find_element_by_xpath("//input[@name='password']")
 submit = driver.find_element_by_xpath("//button[@name='wp-submit']")
@@ -29,11 +33,16 @@ username.send_keys(USERNAME)
 password.send_keys(PASSWORD)
 submit.click()
 
+time.sleep(1)
 driver.get("https://is.vu.lt/pls/klevas/am$pd_reg_app.show?p_stud_id=219671&p_kalba_name=lt")
 page_source = driver.page_source
+url = driver.current_url
 
 f = open("html.txt", "w+")
 f.write(page_source)
+f.close()
+f = open("URL.txt", "w+")
+f.write (url)
 f.close()
 
 driver.close()
